1d89ddbc358ac2b08facdab901d72c53dc506ee3
jamiguel23 <josematthewmiguel@gmail.com>
2021-04-27 21:51:44 -0700

    added question 2

--- js/app.js
+++ js/app.js
@@ -4,14 +4,23 @@
 
 let userName = prompt('What is your name?');
 
-alert('Welcome' + userName + '! Answer the following questions with yes/no or y/n');
+alert('Welcome ' + userName + '! Answer the following questions with yes/no or y/n');
 
 let answerOne = prompt(' Do I live in Seattle?').toLowerCase();
 
 if(answerOne === 'yes' || answerOne === 'y'){
   console.log('you are correct! I live in Seattle');
-  alert('you are correct! I live in Seattle')
-} 
+  alert('you are correct! I live in Seattle');
+// }if(answerOne === 'no'){
+//   console.log('try again');
+//   alert('Wrong! try again.');
+}
 
-let answerTwo = prompt(' Did I used to live in Los Angles, CA?').toLowerCase();
-console.log(answerTwo)
+let answerTwo = prompt(' Did I used to live in San Jose, CA?').toLowerCase();
+
+if(answerTwo === 'no' || answerTwo === 'n'){
+  console.log('Correct! I did NOT ever live in San Jose, CA. I have lived in Oakland, CA');
+  alert( 'Correct! I did NOT ever live in San Jose, CA. I have lived in Oakland, CA');
+}
+
+
