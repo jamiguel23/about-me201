847fe6373e3bd654bcb85ce99b6c365f5cd9bf6b
jamiguel23 <josematthewmiguel@gmail.com>
2021-04-27 22:24:43 -0700

    added questions 3 4 and 5 with more information on html

--- js/app.js
+++ js/app.js
@@ -9,8 +9,9 @@ alert('Welcome ' + userName + '! Answer the following questions with yes/no or y
 let answerOne = prompt(' Do I live in Seattle?').toLowerCase();
 
 if(answerOne === 'yes' || answerOne === 'y'){
-  console.log('you are correct! I live in Seattle');
+  // console.log('you are correct! I live in Seattle');
   alert('you are correct! I live in Seattle');
+// Here I am starting to add more logic and an eventual loop
 // }if(answerOne === 'no'){
 //   console.log('try again');
 //   alert('Wrong! try again.');
@@ -19,8 +20,27 @@ if(answerOne === 'yes' || answerOne === 'y'){
 let answerTwo = prompt(' Did I used to live in San Jose, CA?').toLowerCase();
 
 if(answerTwo === 'no' || answerTwo === 'n'){
-  console.log('Correct! I did NOT ever live in San Jose, CA. I have lived in Oakland, CA');
+  // console.log('Correct! I did NOT ever live in San Jose, CA. I have lived in Oakland, CA');
   alert( 'Correct! I did NOT ever live in San Jose, CA. I have lived in Oakland, CA');
+}
+let answerThree = prompt(' Do I work at Zillow?').toLowerCase();
+
+if(answerThree === 'yes' || answerThree === 'n'){
+  // console.log('DING DING DING! I DO work at Zillow');
+  alert('DING DING DING! I DO work at Zillow');
 }
 
 
+let answerFour = prompt(' Was Chief Seattle the name of my High School?').toLowerCase();
+
+if(answerFour === 'no' || answerFour === 'n'){
+  // console.log('YOU ARE CORRECT! The name if my high school is Chief Sealth');
+  alert('YOU ARE CORRECT! The name if my high school is Chief Sealth');
+}
+
+let answerFive = prompt(' Am I married?').toLowerCase();
+
+if(answerFive === 'yes' || answerFive === 'n'){
+  // console.log('Yes, I am married. #marriedlife');
+  alert('Yes, I am married. #marriedlife');
+}
