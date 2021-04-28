169c031b141e100c3ab174a7c08ddad73755ee2d
jamiguel23 <josematthewmiguel@gmail.com>
2021-04-27 20:39:16 -0700

    added css and JS

--- js/app.js
+++ js/app.js
@@ -1,3 +1,14 @@
 'use strict';
 
-console.log('wazzzzzup world?');
+// console.log('wazzzzzup world?');
+
+let userName = prompt('What is your name?');
+
+alert('Welcome' + userName + '! Answer the following questions with yes/no or y/n');
+
+let answerOne = prompt(' Do I live in Seattle?').toLowerCase();
+
+if(answerOne === 'yes' || answerOne === 'y'){
+  console.log('you are correct! I live in Seattle');
+  alert('you are correct! I live in Seattle')
+} 
