a58ce0391f56953e1be0e03a3fb0bf68a640c47b
jamiguel23 <josematthewmiguel@gmail.com>
2021-05-01 13:01:07 -0700

    added favFoods to HMTL ordered list, added question 6

--- js/app.js
+++ js/app.js
@@ -6,9 +6,9 @@ let userName = prompt('What is your name?');
 
 alert('Welcome ' + userName + '! Answer the following questions with yes/no or y/n');
 
-let answerOne = prompt(' Do I live in Seattle?').toLowerCase();
+let questionOne = prompt(' Do I live in Seattle?').toLowerCase();
 
-if(answerOne === 'yes' || answerOne === 'y'){
+if(questionOne === 'yes' || questionOne === 'y'){
   // console.log('you are correct! I live in Seattle');
   alert('you are correct! I live in Seattle');
 // Here I am starting to add more logic and an eventual loop
@@ -17,15 +17,15 @@ if(answerOne === 'yes' || answerOne === 'y'){
 //   alert('Wrong! try again.');
 }
 
-let answerTwo = prompt(' Did I used to live in San Jose, CA?').toLowerCase();
+let questionTwo = prompt(' Did I used to live in San Jose, CA?').toLowerCase();
 
-if(answerTwo === 'no' || answerTwo === 'n'){
+if(questionTwo === 'no' || questionTwo === 'n'){
   // console.log('Correct! I did NOT ever live in San Jose, CA. I have lived in Oakland, CA');
   alert( 'Correct! I did NOT ever live in San Jose, CA. I have lived in Oakland, CA');
 }
-let answerThree = prompt(' Do I work at Zillow?').toLowerCase();
+let questionThree = prompt(' Do I work at Zillow?').toLowerCase();
 
-if(answerThree === 'yes' || answerThree === 'n'){
+if(questionThree === 'yes' || questionThree === 'n'){
   // console.log('DING DING DING! I DO work at Zillow');
   alert('DING DING DING! I DO work at Zillow');
 }
@@ -46,3 +46,30 @@ if(answerFive === 'yes' || answerFive === 'y'){
 }
 
 alert('Congrats ' + userName + '! Read more on this page to learn more about me' );
+
+
+// Lab 3 notesklo2
+// add 6th question
+alert(`Now we will pay a guessing game. You will guess a number between 1-20 and I will tell you if you are high or low. You have 4 guesses. GOOD LUCK!`)
+
+// respond if too high
+
+let guessingGame = prompt('Guess a number between 1-20');
+
+let attemptsRemaining =3
+let attempts = 7;
+
+if( guessingGame === '12'){
+  console.log(`You are correct!`)
+}
+
+// for (let i =0; i<= attemptsRemaining; i++){
+//   console.log(`I am in the for loop number of attempts remaining: ${attempts - i -1}`);
+// }
+
+
+
+// respond if too low
+// respond if correct AND guesses remain
+// //respond if not correct and no guesses remain
+// increment score if answers correctly
