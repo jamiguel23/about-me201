27fa8ae986fa67b9cfa1bc243cd0474ba691af99
jamiguel23 <josematthewmiguel@gmail.com>
2021-05-06 22:08:08 -0700

    added css styling

--- js/app.js
+++ js/app.js
@@ -2,122 +2,122 @@
 
 console.log('wazzzzzup world?');
 
-let score = 0;
+// let score = 0;
 
-let userName = prompt('What is your name?');
-alert('Welcome ' + userName + '! Answer the following questions with yes/no or y/n');
+// let userName = prompt('What is your name?');
+// alert('Welcome ' + userName + '! Answer the following questions with yes/no or y/n');
 
-function q1() {
-  let questionOne = prompt(' Do I live in Seattle?').toLowerCase();
+// function q1() {
+//   let questionOne = prompt(' Do I live in Seattle?').toLowerCase();
 
-  if (questionOne === 'yes' || questionOne === 'y') {
-    // console.log('you are correct! I live in Seattle');
-    alert('you are correct! I live in Seattle');
-    score++;
-  }
-  else if (questionOne === 'no' || questionOne === 'n') {
-    // console.log('sorry try again');
-    alert('sorry try again');
-  }
-}
-q1();
+//   if (questionOne === 'yes' || questionOne === 'y') {
+//     // console.log('you are correct! I live in Seattle');
+//     alert('you are correct! I live in Seattle');
+//     score++;
+//   }
+//   else if (questionOne === 'no' || questionOne === 'n') {
+//     // console.log('sorry try again');
+//     alert('sorry try again');
+//   }
+// }
+// q1();
 
-function q2() {
-  let questionTwo = prompt(' Did I used to live in San Jose, CA?').toLowerCase();
+// function q2() {
+//   let questionTwo = prompt(' Did I used to live in San Jose, CA?').toLowerCase();
 
-  if (questionTwo === 'no' || questionTwo === 'n') {
-    // console.log('Correct! I did NOT ever live in San Jose, CA. I have lived in Oakland, CA');
-    alert('Correct! I did NOT ever live in San Jose, CA. I have lived in Oakland, CA');
-    score++;
-  }
-  else if (questionTwo === 'yes' || questionTwo === 'y') {
-    // console.log('sorry try again');
-    alert('sorry try again');
-  }
-}
-q2();
+//   if (questionTwo === 'no' || questionTwo === 'n') {
+//     // console.log('Correct! I did NOT ever live in San Jose, CA. I have lived in Oakland, CA');
+//     alert('Correct! I did NOT ever live in San Jose, CA. I have lived in Oakland, CA');
+//     score++;
+//   }
+//   else if (questionTwo === 'yes' || questionTwo === 'y') {
+//     // console.log('sorry try again');
+//     alert('sorry try again');
+//   }
+// }
+// q2();
 
-function q3() {
-  let questionThree = prompt(' Do I work at Zillow?').toLowerCase();
+// function q3() {
+//   let questionThree = prompt(' Do I work at Zillow?').toLowerCase();
 
-  if (questionThree === 'yes' || questionThree === 'y') {
-    // console.log('DING DING DING! I DO work at Zillow');
-    alert('DING DING DING! I DO work at Zillow');
-    score++;
-  }
-  else if (questionThree === 'no' || questionThree === 'n') {
-    // console.log('sorry try again');
-    alert('sorry try again');
-  }
-}
-q3();
+//   if (questionThree === 'yes' || questionThree === 'y') {
+//     // console.log('DING DING DING! I DO work at Zillow');
+//     alert('DING DING DING! I DO work at Zillow');
+//     score++;
+//   }
+//   else if (questionThree === 'no' || questionThree === 'n') {
+//     // console.log('sorry try again');
+//     alert('sorry try again');
+//   }
+// }
+// q3();
 
-function q4() {
-  let answerFour = prompt(' Was Chief Seattle the name of my High School?').toLowerCase();
+// function q4() {
+//   let answerFour = prompt(' Was Chief Seattle the name of my High School?').toLowerCase();
 
-  if (answerFour === 'no' || answerFour === 'n') {
-    // console.log('YOU ARE CORRECT! The name if my high school is Chief Sealth');
-    alert('YOU ARE CORRECT! The name if my high school is Chief Sealth');
-    score++;
-  }
-  else if (answerFour === 'yes' || answerFour === 'y') {
-    // console.log('sorry try again');
-    alert('sorry try again');
-  }
-}
-q4();
+//   if (answerFour === 'no' || answerFour === 'n') {
+//     // console.log('YOU ARE CORRECT! The name if my high school is Chief Sealth');
+//     alert('YOU ARE CORRECT! The name if my high school is Chief Sealth');
+//     score++;
+//   }
+//   else if (answerFour === 'yes' || answerFour === 'y') {
+//     // console.log('sorry try again');
+//     alert('sorry try again');
+//   }
+// }
+// q4();
 
-function q5() {
-  let answerFive = prompt(' Am I married?').toLowerCase();
+// function q5() {
+//   let answerFive = prompt(' Am I married?').toLowerCase();
 
-  if (answerFive === 'yes' || answerFive === 'y') {
-    // console.log('Yes, I am married. #marriedlife');
-    alert('Yes, I am married. Good job ' + userName + ' #marriedlife');
-    score++;
-  }
-  else if (answerFive === 'no' || answerFive === 'n') {
-    // console.log('sorry try again');
-    alert('sorry try again');
-  }
-}
-q5();
-alert('Congrats ' + userName + '! Read more on this page to learn more about me');
+//   if (answerFive === 'yes' || answerFive === 'y') {
+//     // console.log('Yes, I am married. #marriedlife');
+//     alert('Yes, I am married. Good job ' + userName + ' #marriedlife');
+//     score++;
+//   }
+//   else if (answerFive === 'no' || answerFive === 'n') {
+//     // console.log('sorry try again');
+//     alert('sorry try again');
+//   }
+// }
+// q5();
+// alert('Congrats ' + userName + '! Read more on this page to learn more about me');
 
-// Lab 3
-// add 6th question
-alert('Now we will pay a guessing game. You will guess a number between 1-20 and I will tell you if you are high or low. You have 4 guesses. GOOD LUCK!');
+// // Lab 3
+// // add 6th question
+// alert('Now we will pay a guessing game. You will guess a number between 1-20 and I will tell you if you are high or low. You have 4 guesses. GOOD LUCK!');
 
-let attemptsRemaining = 3;
-let attempts = 3;
-let answerSix = 12;
-// created for loop 
+// let attemptsRemaining = 3;
+// let attempts = 3;
+// let answerSix = 12;
+// // created for loop
 
-for (let i = 0; i <= attemptsRemaining; i++) {
-  let questionSix = parseInt(prompt('Guess a number between 1-20'));
-  if (questionSix === answerSix) {
-    console.log('You are correct!');
-    alert('You are correct!');
-    score++;
-    break;
-    // respond if too low
-  } else if (questionSix < answerSix) {
-    console.log('Too low');
-    alert('Too low');
+// for (let i = 0; i <= attemptsRemaining; i++) {
+//   let questionSix = parseInt(prompt('Guess a number between 1-20'));
+//   if (questionSix === answerSix) {
+//     console.log('You are correct!');
+//     alert('You are correct!');
+//     score++;
+//     break;
+//     // respond if too low
+//   } else if (questionSix < answerSix) {
+//     console.log('Too low');
+//     alert('Too low');
 
-    // respond if too high
-  } else if (questionSix > answerSix) {
-    console.log('Too high');
-    alert('Too high');
-  }
-  // //respond if not correct and no guesses remain
-  if (i === attemptsRemaining - 1) {
-    console.log(`The correct number is ${answerSix}`);
-    alert(`Sorry, all attempts are used.The correct number is ${answerSix}`);
-    break;
-  }
+//     // respond if too high
+//   } else if (questionSix > answerSix) {
+//     console.log('Too high');
+//     alert('Too high');
+//   }
+//   // //respond if not correct and no guesses remain
+//   if (i === attemptsRemaining - 1) {
+//     console.log(`The correct number is ${answerSix}`);
+//     alert(`Sorry, all attempts are used.The correct number is ${answerSix}`);
+//     break;
+//   }
 
-}
-// respond if correct AND guesses remain
+// }
+// // respond if correct AND guesses remain
 
 let count = 6;
 // let attemptsQ7 = 6;
@@ -134,9 +134,9 @@ while (count && !answerCorrectly) {
       console.log('Correct!');
       alert('Correct!');
       answerCorrectly = true;
-      score++;
+      // score++;
       break;
-    } else if(questionSeven !== favFoods[i]); {
+    } if (questionSeven !== favFoods[i]); {
       console.log('Incorrect. Try again');
       alert('Incorrect. Try again');
     }
@@ -150,5 +150,5 @@ while (count && !answerCorrectly) {
 
 
 // // increment score if answers correctly
-console.log(`Your score is ${score} out of 7`);
-alert(`Your score is ${score} out of 7`);
+// console.log(`Your score is ${score} out of 7`);
+// alert(`Your score is ${score} out of 7`);
